348ccad5dc8ddafa5dd935c4a4cf75d31cea75d9
路云飞 <luyunfeicx@163.com>
2017-05-10 19:17:59 +0800

    200

--- assignment3.py
+++ assignment3.py
@@ -390,7 +390,7 @@ param_grid = {'n_estimators':range(20,81,10),
 estimator = sklearn.model_selection.GridSearchCV(clf, param_grid)
 estimator.fit(ftrain, ltrain)
 '''
-clf = sklearn.ensemble.GradientBoostingRegressor()
+clf = sklearn.ensemble.GradientBoostingRegressor(n_estimators=200)
 clf.fit(ftrain, ltrain)
 
 print 'Training model finished.'
